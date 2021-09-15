a13ea4de3adf4037b4a7da7f3e60a67ad50852a2
tstocker-mediaschool <thomas.stocker@mediaschool.education>
2021-09-15 17:57:02 +0200

    [FIX] Ajout de la gestion des erreurs

--- webservice/question3.php
+++ webservice/question3.php
@@ -13,10 +13,23 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 // $output contains the output string
 $output = curl_exec($curl);
 
+if ($output === false) {
+    echo 'Erreur Curl : ' . curl_error($curl);
+}
+
 // close curl resource to free up system resources
 // (deletes the variable made by curl_init)
 curl_close($curl);
 
 $data = json_decode($output, true);
 
-echo 'Lattitude: ' . $data['latt'] . ', Longitude: ' . $data['longt'];
+if (!isset($data["error"])) {
+    echo 'Lattitude: ' . $data['latt'] . ', Longitude: ' . $data['longt'];
+} else {
+    echo "Limite de 2 requêtes par seconde dépassée";
+    ?>
+    <pre>
+        <?php print_r($data); ?>
+    </pre>
+    <?php
+}
